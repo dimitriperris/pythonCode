6fc8a2c195eaeb145f3fc99c5ed9dbd2c94602b1
DPerris <dimiperris11@gmail.com>
2023-09-07 11:12:04 -0700

    chnages ch 3 1-6

---
+++ chapter 2/Roman_Numerals.py
@@ -0,0 +1,37 @@
+
+def number_to_roman(num):
+    if num == 1:
+        return "I"
+    elif num == 2:
+        return "II"
+    elif num == 3:
+        return "III"
+    elif num == 4:
+        return "IV"
+    elif num == 5:
+        return "V"
+    elif num == 6:
+        return "VI"
+    elif num == 7:
+        return "VII"
+    elif num == 8:
+        return "VIII"
+    elif num == 9:
+        return "IX"
+    elif num == 10:
+        return "X"
+    else:
+        return None
+
+
+try:
+    user_input = int(input("Enter a number between 1 and 10: "))
+    
+    
+    if 1 <= user_input <= 10:
+        roman_numeral = number_to_roman(user_input)
+        print(f"The Roman numeral for {user_input} is {roman_numeral}.")
+    else:
+        print("Error: Please enter a number within the range of 1 through 10.")
+except ValueError:
+    print("Error: Invalid input. Please enter a valid number.")
---
+++ chapter 2/resturant selector.py
@@ -0,0 +1,31 @@
+# Get dietary preferences from the user
+vegetarian = input("Is anyone in your party a vegetarian? ").lower()
+vegan = input("Is anyone in your party a vegan? ").lower()
+gluten_free = input("Is anyone in your party gluten-free? ").lower()
+
+# Define the restaurant options
+restaurants = {
+    "Joe's Gourmet Burgers": {"vegetarian": "no", "vegan": "no", "gluten_free": "no"},
+    "Main Street Pizza Company": {"vegetarian": "yes", "vegan": "no", "gluten_free": "yes"},
+    "Corner Café": {"vegetarian": "yes", "vegan": "yes", "gluten_free": "yes"},
+    "Mama's Fine Italian": {"vegetarian": "yes", "vegan": "no", "gluten_free": "no"},
+    "The Chef's Kitchen": {"vegetarian": "yes", "vegan": "yes", "gluten_free": "yes"}
+}
+
+# Create a list to store suitable restaurants
+suitable_restaurants = []
+
+# Check dietary preferences and add suitable restaurants to the list
+for restaurant, restrictions in restaurants.items():
+    if (vegetarian == "yes" or restrictions["vegetarian"] == "yes") and \
+       (vegan == "yes" or restrictions["vegan"] == "yes") and \
+       (gluten_free == "yes" or restrictions["gluten_free"] == "yes"):
+        suitable_restaurants.append(restaurant)
+
+# Display suitable restaurant choices
+if suitable_restaurants:
+    print("Here are your restaurant choices:")
+    for restaurant in suitable_restaurants:
+        print(restaurant)
+else:
+    print("Sorry, there are no suitable restaurants for your party's dietary preferences.")
