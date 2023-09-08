d4e01342ac84018b11fc2aec758007f0fbe356a4
DPerris <dimiperris11@gmail.com>
2023-09-08 10:20:44 -0700

    rm ch

--- chapter 2/Roman_Numerals.py
+++ chapter 2/Roman_Numerals.py
@@ -1,37 +1,37 @@
 
 def number_to_roman(num):
     if num == 1:
-        return "I"
+        return 'I'
     elif num == 2:
-        return "II"
+        return 'II'
     elif num == 3:
-        return "III"
+        return 'III'
     elif num == 4:
-        return "IV"
+        return 'IV'
     elif num == 5:
-        return "V"
+        return 'V'
     elif num == 6:
-        return "VI"
+        return 'VI'
     elif num == 7:
-        return "VII"
+        return 'VII'
     elif num == 8:
-        return "VIII"
+        return 'VIII'
     elif num == 9:
-        return "IX"
+        return 'IX'
     elif num == 10:
-        return "X"
+        return 'X'
     else:
         return ()
 
 
 try:
-    user_input = int(input("Enter a number between 1 and 10: "))
+    user_input = int(input('Enter a number between 1 and 10: '))
     
     
     if 1 <= user_input <= 10:
         roman_numeral = number_to_roman(user_input)
-        print(f"The Roman numeral for {user_input} is {roman_numeral}.")
+        print(f'The Roman numeral for {user_input} is {roman_numeral}.')
     else:
-        print("Error: Please enter a number within the range of 1 through 10.")
+        print('Error: Please enter a number within the range of 1 through 10.')
 except ValueError:
-    print("Error: Invalid input, please enter a valid number.")
+    print('Error: Invalid input, please enter a valid number.')
