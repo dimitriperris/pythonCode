ec95e3d6ad3d176b891e2e6e3e4897e122fa49ab
DPerris <dimiperris11@gmail.com>
2023-09-12 10:25:01 -0700

    ch 4

---
+++ chapter 2/Calories_Burned.py
@@ -0,0 +1,42 @@
+# File: Calories_Burned.py
+# Description: The number of calories burned per minute. 
+# Assignment Name and Number: Calories Burned #2
+#
+# Name: Dimitri Perris
+# GitHub: <YOUR GitHub>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+calories_burned_per_minutes= 4.2 
+
+for number_of_minutes in range( 10, 31, 5 ):
+    number_of_calories_burned= ((number_of_minutes /1) * calories_burned_per_minutes)
+    print('You will burn', number_of_calories_burned, 'calories in', number_of_minutes, 'minutes')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
