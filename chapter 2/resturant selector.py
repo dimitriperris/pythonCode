aa83bd77f0d0897c180eb132ce3271bc1ae344d0
DPerris <dimiperris11@gmail.com>
2023-09-12 22:23:22 -0700

    hj

--- chapter 2/resturant selector.py
+++ chapter 2/resturant selector.py
@@ -1,12 +1,12 @@
 print()
-vegitarian= input('Is there anyone in your party that is vegitarian? :')
+vegatarian= input('Is there anyone in your party that is vegatarian? :')
 print()
 vegan= input('Is there anyone in your party that is vegan? :')
 print()
 glutenf= input('Is there anyone in your party that is gluten free? :')
 
-if vegitarian == 'yes' or vegitarian == 'no':
-    if vegitarian == 'yes':
+if vegatarian == 'yes' or vegatarian == 'no':
+    if vegatarian == 'yes':
         if vegan == 'yes':
             if glutenf == 'yes':
                 print('You along with your friends can go to the Chefs Kitchen or the Corner Cafe.')
@@ -14,10 +14,10 @@ if vegitarian == 'yes' or vegitarian == 'no':
                 if glutenf == 'yes':
                     print('You along with your friends can go to the Main Street Pizza Company.')
         else:
-            if vegan == 'no' and vegitarian == 'yes':
+            if vegan == 'no' and vegatarian == 'yes':
                 print('You along with your friends can go to Mamas Fine Italian.')
             else: 
-                if vegitarian == 'no' and vegan == 'no' and glutenf == 'no': 
+                if vegatarian == 'no' and vegan == 'no' and glutenf == 'no': 
                     print('You along with your friends can go to Joes Gourmet Burgers')
     else:
         print('Error, enter either yes or no to proceed.')
