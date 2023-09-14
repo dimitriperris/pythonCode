12546680625d2c2c2cd0c4ba3712bcd800f7fc3d
DPerris <dimiperris11@gmail.com>
2023-09-13 21:04:04 -0700

    file

--- chapter 2/Calc_Factorial_#.py
+++ chapter 2/Calc_Factorial_#.py
@@ -1,3 +1,13 @@
+# File: Calc_Factorial_#.py
+# Description: Calculate the factorial of a nonngeative number.  
+# Assignment Name and Number: Calculate the Factorial of a Number #12
+#
+# Name: Dimitri Perris
+# GitHub: <YOUR GitHub>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 number = int(input("Enter a positive integer:"))
 factorial = 1
 
--- chapter 2/pennies_day.py
+++ chapter 2/pennies_day.py
@@ -1,3 +1,13 @@
+# File: pennies_day.py
+# Description: Calculate the amount of money a person would earn over a period of time in pennies. 
+# Assignment Name and Number: Pennies for Pay #7
+#
+# Name: Dimitri Perris
+# GitHub: <YOUR GitHub>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 number_of_days= int(input('Enter the number of days worked.:'))
 salary= 0.005 
 
