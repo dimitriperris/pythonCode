460a4cc7b48189b320da30ea261336e4b3ab28ad
DPerris <dimiperris11@gmail.com>
2023-09-25 11:20:53 -0700

    ssd

--- chapter 4/pennies_day.py
+++ chapter 4/pennies_day.py
@@ -9,11 +9,11 @@
 # and I have not provided this code to any other student.
 
 number_of_days= int(input('Enter the number of days worked.:'))
-salary= 0.005 
+salary= 0.01
 
 for day in range(1, number_of_days + 1):
     salary *= 2.0
-    print('Day', day, 'salary', salary)
-print('Your salary on day', number_of_days, 'would be', salary)
+    print('Day', day, 'salary $', salary)
+print('Your salary on day', number_of_days, 'would be $', salary)
           
 
