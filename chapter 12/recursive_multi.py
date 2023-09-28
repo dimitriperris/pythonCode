ac4cfba8c0f0a0bfb78b026353a9d096762c448c
DPerris <dimiperris11@gmail.com>
2023-09-27 18:57:08 -0700

    nn

--- chapter 12/recursive_multi.py
+++ chapter 12/recursive_multi.py
@@ -1,3 +1,13 @@
+# File: recursive_multi.py
+# Description: Use recurisve to multiply two arguments. 
+# Assignment Name and Number: Recursive Multiplication #2
+#
+# Name: Dimitri Perris
+# GitHub: <YOUR GitHub>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 def multiply_recursive(x, y):
     if y == 1:
         return x
