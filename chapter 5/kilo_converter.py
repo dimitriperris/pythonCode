e4c5697d62ba48162abc41df6c7af52a881f883d
DPerris <dimiperris11@gmail.com>
2023-10-20 11:27:50 -0700

    wcef

--- chapter 5/kilo_converter.py
+++ chapter 5/kilo_converter.py
@@ -8,16 +8,18 @@
 # On my honor, <YOUR NAME>, this programming assignment is my own work
 # and I have not provided this code to any other student.
 
-def kilo_miles(kilometers):
-    miles = kilometers * 0.6214
+def main():
+    global kilo
+    kilo = int(input('Enter the distance in kilometers: '))
+    final= kilo_miles()
+
+    print('The conversion of', kilo, 'kilometers to miles is', final, 'miles.')
+
+def kilo_miles():
+    miles = kilo * 0.6214
     return miles
 
-def main():
-    try:
-        kilometers = float(input('Enter a distance in kilometers: '))
-        miles = kilo_miles(kilometers)
-        print('Kilometers', kilometers, 'is equal to miles', miles)
-    except ValueError:
-        print('Error. Please enter a correct number for kilometers.')
+
+
 
 main()
--- chapter 5/paint_job.py
+++ chapter 5/paint_job.py
@@ -8,20 +8,27 @@
 # On my honor, <YOUR NAME>, this programming assignment is my own work
 # and I have not provided this code to any other student.
 
+import math
+
 def main():
     sqr_ft = float(input('Enter the square feet of wall space to be painted: '))
     paint_price = float(input('Enter the price of paint per gallon: '))
 
+    results(sqr_ft, paint_price)
+
+def results(sqr_ft, paint_price):
     gallons_required = sqr_ft / 112
     hours_required = gallons_required * 8
     paint_cost = gallons_required * paint_price
     labor_charges = hours_required * 35
     total_cost = paint_cost + labor_charges
 
-    print('Gallons of paint required', gallons_required)
-    print('Hours of labor required', hours_required)
-    print('Cost of the paint', paint_cost)
-    print('Labor charges', labor_charges)
-    print('Total cost of the paint job', total_cost)
+    print('Gallons of paint required', format(gallons_required, '.2f'))
+    print('Hours of labor required', format(hours_required, '.2f'))
+    print('Cost of the paint', format(paint_cost, '.2f'))
+    print('Labor charges', format(labor_charges, '.2f'))
+    print('Total cost of the paint job', format(total_cost, '.2f'))
 
 main()
+
+
